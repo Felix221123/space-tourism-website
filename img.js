a18bdb61c29d210667f2f40034bbd8d4b3538c93
Felix <felixbaah47@gmail.com>
2023-12-03 19:35:50 +0000

    media query changes

--- img.js
+++ img.js
@@ -2,7 +2,7 @@ const techImage = document.getElementById('tech-image-style')
 
 const imageScreebSize = window.innerWidth
 if (imageScreebSize >= 1439) {
-    techImage.src = '/assets/technology/image-launch-vehicle-landscape.jpg'
+    techImage.src = 'starter-code/assets/technology/image-launch-vehicle-landscape.jpg'
 }
 
 
